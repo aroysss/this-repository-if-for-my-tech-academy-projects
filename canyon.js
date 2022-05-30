2fd89f27ed6557d07490653046c6611a9a3ed548
aroysss <100801481+aroysss@users.noreply.github.com>
2022-05-30 15:20:21 -0500

    Delete canyon.js

--- canyon.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("a[href^="#"]").forEach(anchor => {
-	anchor.addEventListener("click", function(e){
-		document.querySelector(this.getAttribute("href")).scrollIntoView({
-			behavior : "smooth"
-		});
-	});
-});
